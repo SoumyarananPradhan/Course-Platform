55173e5b8d5a6e2e99bebc4e43249fd51e21a00d
Soumyaranjan <soumyaranjanaman54@gmail.com>
2025-11-22 15:28:23 +0530

    Make Migrations and Migrate with Django

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 
 
-class Access_Requirement(model.TextChoices):
+class Access_Requirement(models.TextChoices):
     ANYONE = "any", "Anyone"
     EMAIL_REQUIRED = "email_required","Email_Required"
 
-class Publish_Status(model.TextChoices):
+class Publish_Status(models.TextChoices):
     PUBLISHED = "publish", "Published"
     COMMING_SOON = "soon","Coming Soon"
     DRAFT = "draft", "Draft"
 
-class Course(model.Model):
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    # image = 
-    access = model.CharField(max_length=10, choices=Access_Requirement.choices,
-    default=Access_Requirement.DRAFT)
-    status = model.CharField(max_length=10, choices=Publish_Status.choices,
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    access = models.CharField(max_length=15, choices=Access_Requirement.choices,
+    default=Access_Requirement.EMAIL_REQUIRED)
+    status = models.CharField(max_length=10, choices=Publish_Status.choices,
     default=Publish_Status.DRAFT)
 
 @property
